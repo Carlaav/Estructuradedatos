b23b4b8261bf3a2d5875669af22dde736c8d2452
Carlaav <carla2002.av@gmail.com>
2022-04-11 21:16:26 +0200

    a

--- 3.py
+++ 3.py
@@ -1,1 +1,28 @@
+from enum import Enum
+
+
+class Naturaleza(Enum):
+    ALIMENTARIA = 0.055
+    SERVICIO = 0.2
+
+class Producto():
+    def __init__(self, tipo) -> None:
+        self.porcentaje = tipo.value
+        self.precio = 100
+    
+    def facturar(self):
+        return self.precio + (self.precio * self.porcentaje)
+
+class FactoryFactura():
+    def crear(self, producto):
+        self.producto = producto
+        return self.producto
+
+    
+
+
+producto = Producto(Naturaleza.SERVICIO)
+factura = FactoryFactura()
+precio_neto = factura.crear(producto).facturar()
+print(precio_neto)
 
